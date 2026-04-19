5add0c8fcc4db4b1f8192ec3136eb10715407c43
0xdefacedd <0xdefacedd@proton.me>
2026-04-20 03:14:38 +1000

    added fxml/controller bones for quiz question
    + added quiz controller and fxml files
    - commented out some unused methods in HomeController pending removal

--- src/main/java/com/codependentvariables/aiandme/controller/HomeController.java
+++ src/main/java/com/codependentvariables/aiandme/controller/HomeController.java
@@ -1,18 +1,12 @@
 package com.codependentvariables.aiandme.controller;
 
-import com.codependentvariables.aiandme.AiAndMe;
 import com.codependentvariables.aiandme.navigation.Router;
 import com.codependentvariables.aiandme.navigation.View;
-import com.codependentvariables.aiandme.state.AppState;
 import javafx.fxml.FXML;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.SVGPath;
-
-import static javafx.application.Application.setUserAgentStylesheet;
 
 public class HomeController {
-    private final AppState appState = AppState.getInstance();
-
+/*          //TODO: remove these unused methods once I determine this isn't the right place for them anymore
+            //TODO: ...same with those dependencies up there too ^^
     @FXML
     private void navigateQuizLibrary() {
         Router.navigateLayout(View.QUIZ_LIBRARY);
@@ -26,5 +20,6 @@ public class HomeController {
     @FXML
     private void navigateLogin() {
         Router.navigateApp(View.LOGIN);
-    }
+    }*/
+
 }
