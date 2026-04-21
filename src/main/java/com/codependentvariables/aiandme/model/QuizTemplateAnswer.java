2e46eb42858252c178b9189e4b7908c30c439b19
Jonte Te Moni <jonte.temoni@connect.qut.edu.au>
2026-04-21 23:43:07 +1000

    Feat (QuizTemplateAnswer) Added Java, DAO, Interface, and SqliteDAO #116

---
+++ src/main/java/com/codependentvariables/aiandme/model/QuizTemplateAnswer.java
@@ -0,0 +1,41 @@
+package com.codependentvariables.aiandme.model;
+
+public class QuizTemplateAnswer {
+    private int id;
+    private int quizTemplateQuestionId;
+    private String text;
+    private byte[] image; // nullable
+    private boolean isCorrect;
+
+    public QuizTemplateAnswer(int quizTemplateQuestionId, String text, boolean isCorrect) {
+        this.quizTemplateQuestionId = quizTemplateQuestionId;
+        this.text = text;
+        this.isCorrect = isCorrect;
+    }
+
+    public QuizTemplateAnswer(int quizTemplateQuestionId, String text, byte[] image, boolean isCorrect) {
+        this.quizTemplateQuestionId = quizTemplateQuestionId;
+        this.text = text;
+        this.image = image;
+        this.isCorrect = isCorrect;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getQuizTemplateQuestionId() { return quizTemplateQuestionId; }
+    public void setQuizTemplateQuestionId(int quizTemplateQuestionId) { this.quizTemplateQuestionId = quizTemplateQuestionId; }
+
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+
+    public byte[] getImage() { return image; }
+    public void setImage(byte[] image) { this.image = image; }
+
+    public boolean isCorrect() { return isCorrect; }
+    public void setCorrect(boolean correct) { isCorrect = correct; }
+
+    @Override
+    public String toString() { return text; }
+}
+
