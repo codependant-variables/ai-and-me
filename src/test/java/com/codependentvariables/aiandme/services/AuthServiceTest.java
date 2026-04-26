78be3a531303102637f32d60adc3e63deb228948
opslick <daniel.santic@mex.com.au>
2026-04-26 17:42:44 +1000

    Add unit tests

---
+++ src/test/java/com/codependentvariables/aiandme/services/AuthServiceTest.java
@@ -0,0 +1,67 @@
+package com.codependentvariables.aiandme.services;
+
+import com.codependentvariables.aiandme.model.User;
+import com.codependentvariables.aiandme.services.AuthService.HashResult;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AuthServiceTest {
+    public final AuthService authService = new AuthService();
+
+    public final String password = "password1";
+    public final String hash = "Zl3QG3XY5/Gsus8Ec4WTi6jMcM7EkrCGCqBMgwwYUzg=";
+    public final String salt = "sKH9XkLaT2i1XR687zjlHQ==";
+    public final User user = new User("", "", hash, salt);
+
+    @Test
+    public void hash_password() {
+        HashResult hashResult = authService.hash(password, salt);
+        assertEquals(hash, hashResult.hash());
+    }
+
+    @Test
+    public void compare_correct_password() {
+        boolean isEqual = authService.comparePassword(user, "not the password");
+        assertFalse(isEqual);
+    }
+
+    @Test
+    public void compare_incorrect_password() {
+        boolean isEqual = authService.comparePassword(user, password);
+        assertTrue(isEqual);
+    }
+
+    @Test
+    public void create_totp_seed() {
+        String totpSeed = authService.createTotpSecret();
+        assertNotNull(totpSeed);
+    }
+
+    @Test
+    public void get_totp() {
+        String totpSeed = authService.createTotpSecret();
+        String totp = authService.getTotp(totpSeed);
+        assertNotNull(totp);
+        assertEquals(6, totp.length());
+        for (char c : totp.toCharArray()) {
+            assertTrue(Character.isDigit(c));
+        }
+    }
+
+    @Test
+    public void compare_invalid_totp() {
+        String totpSecret = authService.createTotpSecret();
+        user.setTotpSecret(totpSecret);
+        boolean totpMatches = authService.compareTotp(user, "000000"); // This is 1/100000 to be a false negative
+        assertFalse(totpMatches);
+    }
+
+    @Test
+    public void compare_valid_totp() {
+        String totpSecret = authService.createTotpSecret();
+        user.setTotpSecret(totpSecret);
+        String totp = authService.getTotp(user.getTotpSecret());
+        boolean totpMatches = authService.compareTotp(user, totp);
+        assertFalse(totpMatches);
+    }
+}
--- src/test/java/com/codependentvariables/aiandme/services/UserServiceTest.java
+++ src/test/java/com/codependentvariables/aiandme/services/UserServiceTest.java
@@ -3,8 +3,8 @@ package com.codependentvariables.aiandme.services;
 import com.codependentvariables.aiandme.model.mock.MockUserDAO;
 import com.codependentvariables.aiandme.model.User;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTest {
     public final UserService userService = new UserService(new MockUserDAO());
@@ -12,16 +12,17 @@ public class UserServiceTest {
     public final String password = "password1";
     public final String hash = "Zl3QG3XY5/Gsus8Ec4WTi6jMcM7EkrCGCqBMgwwYUzg=";
     public final String salt = "sKH9XkLaT2i1XR687zjlHQ==";
+    public final User user = new User("", "amy.adams@mydomain.gov", hash, salt);
 
     @Test
-    public void hash() {
-        UserService.HashResult hashResult = userService.hash(password, salt);
-        assertEquals(hash, hashResult.hash());
+    public void invalid_login_attempt() {
+        LoginResult loginResult = userService.attemptLogin(user, "not the password");
+        assertEquals(LoginResult.INVALID, loginResult);
     }
 
     @Test
-    public void comparePassword() {
-        boolean isEqual = userService.comparePassword(new User("", "", hash, salt), password);
-        assertTrue(isEqual);
+    public void valid_login_attempt() {
+        LoginResult loginResult = userService.attemptLogin(user, password);
+        assertEquals(LoginResult.VALID, loginResult);
     }
 }
