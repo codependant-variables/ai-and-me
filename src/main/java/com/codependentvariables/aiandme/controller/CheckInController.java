1bfcc9387e034732a8676f2a750f5f4059bacd17
opslick <daniel.santic@mex.com.au>
2026-04-27 03:15:28 +1000

    Make checkin home button shared and use theme

--- src/main/java/com/codependentvariables/aiandme/controller/CheckInController.java
+++ src/main/java/com/codependentvariables/aiandme/controller/CheckInController.java
@@ -1,10 +1,24 @@
 package com.codependentvariables.aiandme.controller;
 
+import com.codependentvariables.aiandme.Svg;
 import com.codependentvariables.aiandme.navigation.Router;
 import com.codependentvariables.aiandme.navigation.View;
+import com.codependentvariables.aiandme.state.AppState;
 import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
 
 public class CheckInController {
+    private final AppState appState = AppState.getInstance();
+
+    @FXML
+    public SVGPath homeSvg;
+
+    @FXML
+    public void initialize() {
+        homeSvg.setContent(Svg.HOME);
+        homeSvg.setFill(appState.getIsDarkMode() ? Color.WHITE : Color.BLACK);
+    }
 
     @FXML
     public void navigateHome() {
