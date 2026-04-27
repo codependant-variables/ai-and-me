ebd3204e466ce4d2b18a9d83d48c3e7fa8bb1025
Will Lucas <willlucas00@gmail.com>
2026-04-27 15:33:19 +1000

    Added ON DELETE CASCADE to Checkin and User DAO schema

--- src/main/java/com/codependentvariables/aiandme/model/dao/SqliteCheckinDAO.java
+++ src/main/java/com/codependentvariables/aiandme/model/dao/SqliteCheckinDAO.java
@@ -15,6 +15,7 @@ public class SqliteCheckinDAO extends BaseSqliteDAO implements ICheckinDAO, IDat
                 ai_happiness decimal NOT NULL,
                 ai_dependency decimal NOT NULL,
                 completed_at datetime NOT NULL
+                ON DELETE CASCADE
             );
         """;
 
--- src/main/java/com/codependentvariables/aiandme/model/dao/SqliteUserDAO.java
+++ src/main/java/com/codependentvariables/aiandme/model/dao/SqliteUserDAO.java
@@ -16,6 +16,7 @@ public class SqliteUserDAO extends BaseSqliteDAO implements IUserDAO, IDatabaseE
                 totp_secret CHAR(30),
                 is_dark_mode BIT NOT NULL DEFAULT FALSE,
                 is_vertical BIT NOT NULL DEFAULT FALSE
+                ON DELETE CASCADE
             );
         """;
 
