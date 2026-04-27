b1fa5022d48a92660f93a327f381b33b81ac0870
Emily (Emma) Robinson <e26.robinson@connect.qut.edu.au>
2026-04-27 16:13:31 +1000

    Fix MockCheckinDAO to return test data and prevent null in get() test

--- src/test/java/com/codependentvariables/aiandme/model/mock/MockCheckinDAO.java
+++ src/test/java/com/codependentvariables/aiandme/model/mock/MockCheckinDAO.java
@@ -3,12 +3,25 @@ package com.codependentvariables.aiandme.model.mock;
 import com.codependentvariables.aiandme.model.Checkin;
 import com.codependentvariables.aiandme.model.dao.ICheckinDAO;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MockCheckinDAO implements ICheckinDAO {
     private final List<Checkin> checkins = new ArrayList<>();
 
+    public MockCheckinDAO() {
+        Checkin checkin = new Checkin(
+                6.4f,
+                5.0f,
+                5.0f,
+                LocalDateTime.parse("2026-04-14T17:28:00")
+        );
+        checkin.setId(1);
+        checkin.setUserId(1);
+
+        checkins.add(checkin);
+    }
     @Override
     public void add(Checkin checkin) {
         checkins.add(checkin);
