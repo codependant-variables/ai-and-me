1bc3f2b7fb84926b9d7d934cdf44b3ae05dc132b
Jonte Te Moni <jonte.temoni@connect.qut.edu.au>
2026-04-28 21:32:39 +1000

    Feat (QuizTemplateAnswer) Added Exception handling for answer related user inputs #195, #200, #206

--- src/main/java/com/codependentvariables/aiandme/model/QuizTemplateAnswer.java
+++ src/main/java/com/codependentvariables/aiandme/model/QuizTemplateAnswer.java
@@ -8,12 +8,14 @@ public class QuizTemplateAnswer {
     private boolean isCorrect;
 
     public QuizTemplateAnswer(int quizTemplateQuestionId, String text, boolean isCorrect) {
+        if (text == null || text.isBlank()) throw new IllegalArgumentException("text must not be blank");
         this.quizTemplateQuestionId = quizTemplateQuestionId;
         this.text = text;
         this.isCorrect = isCorrect;
     }
 
     public QuizTemplateAnswer(int quizTemplateQuestionId, String text, byte[] image, boolean isCorrect) {
+        if (text == null || text.isBlank()) throw new IllegalArgumentException("text must not be blank");
         this.quizTemplateQuestionId = quizTemplateQuestionId;
         this.text = text;
         this.image = image;
