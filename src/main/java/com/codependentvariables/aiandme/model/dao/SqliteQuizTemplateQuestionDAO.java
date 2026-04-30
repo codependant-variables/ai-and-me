d882bb2a2884b6293d40bbb30d8aa178a21cbf9a
Jonte Te Moni <jonte.temoni@connect.qut.edu.au>
2026-05-01 01:24:22 +1000

    Feat (QuizLibrary) Added Example Questions inline with #164
    Prepared for the Presentation

--- src/main/java/com/codependentvariables/aiandme/model/dao/SqliteQuizTemplateQuestionDAO.java
+++ src/main/java/com/codependentvariables/aiandme/model/dao/SqliteQuizTemplateQuestionDAO.java
@@ -21,7 +21,42 @@ public class SqliteQuizTemplateQuestionDAO extends BaseSqliteDAO implements IQui
     public String getSchemaQuery() { return schemaQuery; }
 
     @Override
-    public String getSeedDataQuery() { return ""; }
+    public String getSeedDataQuery() {
+        return """
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; 2 + 3 * 6 =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; 4 + 5 - 4 / 2 =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; 3 * 4 - 6 / 2 =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; 4 - ( 2 * 4 ) / 2 + 6 =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; ( 81 / 9 ) + 2 * 4 =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; 14 / 2 * ( 5 + 2 ) =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; 50 / 4 * 2 + (17 + 8) =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; 6 * (2 + 3^2) - 4 =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; 42 / ( ( 141 / ( 11 * 4 + 3) ) + 4 ) =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (1, 'Solve the Equation; 8 * 6 + 12 - ( 4^2 + (3^4 / 40.5 ) * 5 ) =');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 3, 7, 11, ?, 19, 23');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 1, ?, -2, -6, -14, -30');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 60, 74.5, ?, 103.5, 118');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 4, ?, 36, 108, 324');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 5, 7, 12, 19, ?, 50, 81');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 2, ?, 4, 6, 8, 12, 18, 26, 38');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 11, 13, 17, 19, 23, ?, 31');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 2, ?, -2, 2, -4, -8, 32, -256');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 1, 4, 9, 16, ?, 32');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (2, 'Identify the missing value; 0, 15, ?, 7.5, 5, 3,');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Apple, Orange, Carrot, Banana, Lemon');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Plate, Brisket, Flank, Jowl, Scale');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; France, Spain, Bulgaria, Peru, Iceland');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Puffer, Hourglass, Bream, Flathead, Mullet');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Pacific, Indian, Atlantic, Arctic, Amazon');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Aquifer, Mountain, Tor, Mesa, Butte');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Maple, Bamboo, Willow, Sycamore, Birch');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Frog, Turtle, Crocodile, Chameleon, Snake');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Radio, Infrared, Gamma, Microwave, Remote');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Lungs, Heart, Muscles, Skin, Stomach');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Bus, Car, Bicycle, Plane, Skateboard');
+            INSERT INTO quiz_template_questions (quiz_template_id, text) VALUES (3, 'Identify the Outlier amongst these words; Gold, Iron, Copper, Aluminum, Lead');
+        """;
+    }
 
     private static final IRowMapper<QuizTemplateQuestion> MAPPER = (rs) -> {
         QuizTemplateQuestion question = new QuizTemplateQuestion(
