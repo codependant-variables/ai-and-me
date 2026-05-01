4b4827dc69837ecd823b5dc4dd0ebddb1d9b7532
Will Lucas <willlucas00@gmail.com>
2026-05-01 11:46:47 +1000

    PLEASE WORK I BEG

--- src/main/java/com/codependentvariables/aiandme/database/SqliteConnection.java
+++ src/main/java/com/codependentvariables/aiandme/database/SqliteConnection.java
@@ -56,7 +56,7 @@ public class SqliteConnection {
                 new SqliteQuizAttemptDAO(),
                 new SqliteQuizAttemptQuestionDAO(),
                 new SqliteQuizAttemptAnswerDAO(),
-                new SqlitePreferredCategoryDAO()
+                new SqlitePreferredCategoryDAO(),
                 new SqliteQuizAttemptAnswerDAO()
         };
 
