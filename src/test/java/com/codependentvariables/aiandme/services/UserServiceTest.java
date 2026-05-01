423ce7b70b4014ad5887d652d5bcf1ae12a20629
opslick <daniel.santic@mex.com.au>
2026-05-01 17:52:29 +1000

    More fixes

--- src/test/java/com/codependentvariables/aiandme/services/UserServiceTest.java
+++ src/test/java/com/codependentvariables/aiandme/services/UserServiceTest.java
@@ -2,12 +2,17 @@ package com.codependentvariables.aiandme.services;
 
 import com.codependentvariables.aiandme.model.mock.MockUserDAO;
 import com.codependentvariables.aiandme.model.User;
+import com.codependentvariables.aiandme.state.AppState;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 public class UserServiceTest {
     public final UserService userService = new UserService(new MockUserDAO());
+    public final AppState appState = AppState.getInstance();
 
     public final String password = "password1";
     public final String hash = "Zl3QG3XY5/Gsus8Ec4WTi6jMcM7EkrCGCqBMgwwYUzg=";
@@ -24,5 +29,20 @@ public class UserServiceTest {
     public void valid_login_attempt() {
         LoginResult loginResult = userService.attemptLogin(user, password);
         assertEquals(LoginResult.VALID, loginResult);
+    }
+
+    @Test
+    public void delete_current_user() {
+        appState.setCurrentUser(user);
+        userService.deleteCurrentUser();
+
+        assertNull(appState.getCurrentUser());
+    }
+
+    @Test
+    public void delete_current_user_without_current_user() {
+        appState.setCurrentUser(null);
+        userService.deleteCurrentUser();
+        assertNull(appState.getCurrentUser());
     }
 }
