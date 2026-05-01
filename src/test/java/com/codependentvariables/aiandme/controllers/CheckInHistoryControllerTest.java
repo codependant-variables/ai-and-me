ea1dd49fa32d576822877ea3e0e4932893f2c031
Euan Campbell <euan.campbell@connect.qut.edu.au>
2026-05-01 10:46:29 +1000

    Update Unit Test for check in History

--- src/test/java/com/codependentvariables/aiandme/controllers/CheckInHistoryControllerTest.java
+++ src/test/java/com/codependentvariables/aiandme/controllers/CheckInHistoryControllerTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// Goal: ensure the controller can be instantiated with a mock DAO
+// Unit Test - Ensure the controller can be instantiated with a mock DAO
 public class CheckInHistoryControllerTest {
 
     @BeforeEach
