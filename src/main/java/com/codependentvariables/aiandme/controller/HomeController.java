94daaa2322b6226bb19e512ee31612cb12818aa7
Will Lucas <willlucas00@gmail.com>
2026-05-08 20:29:47 +1000

    modified HomeController to show last check-in date

--- src/main/java/com/codependentvariables/aiandme/controller/HomeController.java
+++ src/main/java/com/codependentvariables/aiandme/controller/HomeController.java
@@ -3,14 +3,18 @@ package com.codependentvariables.aiandme.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import com.codependentvariables.aiandme.services.HomeService;
+import javafx.scene.text.Text;
 
 public class HomeController {
     @FXML
     private Label checkInStreak;
+    @FXML
+    private Text lastCheckIn;
 
     private final HomeService homeService = HomeService.getInstance();
 
     public void initialize() {
         checkInStreak.setText(homeService.getCheckInStreak());
+        lastCheckIn.setText(homeService.getLastCheckInDate());
     }
 }
