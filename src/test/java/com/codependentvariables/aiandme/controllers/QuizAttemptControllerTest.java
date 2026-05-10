949fe07b1862d6cd5bdcaf8897d3c8e1cfaf8bec
Will Lucas <willlucas00@gmail.com>
2026-05-10 19:27:59 +1000

    feat: added unfinished methods for tests 2 and 3

--- src/test/java/com/codependentvariables/aiandme/controllers/QuizAttemptControllerTest.java
+++ src/test/java/com/codependentvariables/aiandme/controllers/QuizAttemptControllerTest.java
@@ -1,9 +1,21 @@
 package com.codependentvariables.aiandme.controllers;
 
+import com.codependentvariables.aiandme.controller.QuizAttemptController;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class QuizAttemptControllerTest {
+
     // 1 - Check that a loaded quiz is being attempted loads the next question and answers properly.
 
     // 2 - Going to the next question works
-
+    @Test
+    void testHandleNext() {
+        fail("Not implemented.");
+    }
     // 3 - At the last question, prompt for completion
+    @Test
+    void testFinishQuiz() {
+        fail("Not implemented.");
+    }
 }
