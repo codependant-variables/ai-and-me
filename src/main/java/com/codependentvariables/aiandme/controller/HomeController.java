0b6929501ddc2f58938849c9bf07191ba8587392
Euan Campbell <euan.campbell@connect.qut.edu.au>
2026-05-10 21:00:02 +1000

    Remove CheckIn button, link page With Dashbaord

--- src/main/java/com/codependentvariables/aiandme/controller/HomeController.java
+++ src/main/java/com/codependentvariables/aiandme/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.codependentvariables.aiandme.controller;
 
+import com.codependentvariables.aiandme.navigation.Router;
+import com.codependentvariables.aiandme.navigation.View;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import com.codependentvariables.aiandme.services.HomeService;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class HomeController {
@@ -16,5 +19,9 @@ public class HomeController {
     public void initialize() {
         checkInStreak.setText(homeService.getCheckInStreak());
         lastCheckIn.setText(homeService.getLastCheckInDate());
+    }
+
+    public void navigateCheckIn(MouseEvent mouseEvent) {
+        Router.navigateLayout(View.CHECK_IN);
     }
 }
