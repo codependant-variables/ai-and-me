5d524674492d45e9e373e0fabb1bb1e797c29733
Jonte Te Moni <jonte.temoni@connect.qut.edu.au>
2026-05-10 19:11:56 +1000

    Feat (QuizAttemptControllerTest) Added file

---
+++ src/test/java/com/codependentvariables/aiandme/controllers/QuizAttemptControllerTest.java
@@ -0,0 +1,9 @@
+package com.codependentvariables.aiandme.controllers;
+
+public class QuizAttemptControllerTest {
+    // 1 - Check that a loaded quiz is being attempted loads the next question and answers properly.
+
+    // 2 - Going to the next question works
+
+    // 3 - At the last question, prompt for completion
+}
