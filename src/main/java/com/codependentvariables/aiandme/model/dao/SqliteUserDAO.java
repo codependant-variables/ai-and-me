795ff66df5d59e9fac8087e47c9a8ddb6efac7d9
Will Lucas <willlucas00@gmail.com>
2026-05-11 17:16:56 +1000

    Added updateLastLoginAt method to IUserDAO, SqliteUserDAO and MockUserDAO

--- src/main/java/com/codependentvariables/aiandme/model/dao/SqliteUserDAO.java
+++ src/main/java/com/codependentvariables/aiandme/model/dao/SqliteUserDAO.java
@@ -18,13 +18,13 @@ public class SqliteUserDAO extends BaseSqliteDAO implements IUserDAO, IDatabaseE
                 totp_secret CHAR(30),
                 is_dark_mode BIT NOT NULL DEFAULT FALSE,
                 is_vertical BIT NOT NULL DEFAULT FALSE,
-                last_activity TIMESTAMP NOT NULL
+                last_login_at TIMESTAMP NOT NULL
             );
         """;
 
     private static final String seedDataQuery = """ 
-            INSERT INTO users (name, email, password, salt, last_activity) VALUES ('Amy Adams', 'amy.adams@mydomain.gov', 'Zl3QG3XY5/Gsus8Ec4WTi6jMcM7EkrCGCqBMgwwYUzg=', 'sKH9XkLaT2i1XR687zjlHQ==', '1778475998777');
-            INSERT INTO users (name, email, password, salt, last_activity) VALUES ('Bob Builder', 'bobthebulider23@swagmail.net', 'Qf15LlrXz/ghNuMGZG3heBeqH3xeuzITnsRhHTDxzR4=', '0akeeTvljQojvcWqb4cg/Q==', '1778475998777');
+            INSERT INTO users (name, email, password, salt, last_login_at) VALUES ('Amy Adams', 'amy.adams@mydomain.gov', 'Zl3QG3XY5/Gsus8Ec4WTi6jMcM7EkrCGCqBMgwwYUzg=', 'sKH9XkLaT2i1XR687zjlHQ==', '1778475998777');
+            INSERT INTO users (name, email, password, salt, last_login_at) VALUES ('Bob Builder', 'bobthebulider23@swagmail.net', 'Qf15LlrXz/ghNuMGZG3heBeqH3xeuzITnsRhHTDxzR4=', '0akeeTvljQojvcWqb4cg/Q==', '1778475998777');
         """;
 
     public String getSchemaQuery() {
@@ -43,7 +43,8 @@ public class SqliteUserDAO extends BaseSqliteDAO implements IUserDAO, IDatabaseE
                 resultSet.getString("salt"),
                 resultSet.getString("totp_secret"),
                 resultSet.getBoolean("is_dark_mode"),
-                resultSet.getBoolean("is_vertical")
+                resultSet.getBoolean("is_vertical"),
+                resultSet.getTimestamp("last_login_at")
         );
         user.setId(resultSet.getInt("id"));
         return user;
@@ -51,7 +52,7 @@ public class SqliteUserDAO extends BaseSqliteDAO implements IUserDAO, IDatabaseE
 
     @Override
     public void add(User user) {
-        final String query = "INSERT INTO users (name, email, password, salt, totp_secret, is_dark_mode, is_vertical, last_activity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        final String query = "INSERT INTO users (name, email, password, salt, totp_secret, is_dark_mode, is_vertical, last_login_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         int id = executeSqlWithGeneratedKeys(query,
                 statement -> {
                     statement.setString(1, user.getName());
@@ -61,14 +62,14 @@ public class SqliteUserDAO extends BaseSqliteDAO implements IUserDAO, IDatabaseE
                     statement.setString(5, user.getTotpSecret());
                     statement.setBoolean(6, user.getIsDarkMode());
                     statement.setBoolean(7, user.getIsVertical());
-                    statement.setTimestamp(8, Timestamp.valueOf(LocalDateTime.now()));
+                    statement.setTimestamp(8, user.getLastLoginAt());
                 });
         user.setId(id);
     }
 
     @Override
     public void update(User user) {
-        final String query = "UPDATE users SET name = ?, email = ?, password = ?, salt = ?, totp_secret = ?, is_dark_mode = ?, is_vertical = ?, last_activity = ? WHERE id = ?";
+        final String query = "UPDATE users SET name = ?, email = ?, password = ?, salt = ?, totp_secret = ?, is_dark_mode = ?, is_vertical = ? WHERE id = ?";
 
         executeSql(query,
                 statement -> {
@@ -79,9 +80,21 @@ public class SqliteUserDAO extends BaseSqliteDAO implements IUserDAO, IDatabaseE
                     statement.setString(5, user.getTotpSecret());
                     statement.setBoolean(6, user.getIsDarkMode());
                     statement.setBoolean(7, user.getIsVertical());
-                    statement.setTimestamp(8, Timestamp.valueOf(LocalDateTime.now()));
-                    statement.setInt(9, user.getId());
+                    statement.setInt(8, user.getId());
                 });
+    }
+
+    @Override
+    public void updateLastLoginAt(User user) {
+        Timestamp lastLoginAt = Timestamp.valueOf(LocalDateTime.now());
+        String query = "UPDATE users SET last_login_at = ? WHERE id = ?";
+
+        executeSql(query, statement -> {
+            statement.setTimestamp(1, lastLoginAt);
+            statement.setInt(2, user.getId());
+        });
+        user.setLastLoginAt(lastLoginAt);
+        System.out.println("Updated last login");
     }
 
     @Override
